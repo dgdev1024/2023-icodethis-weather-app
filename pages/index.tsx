ee6bb911ea0ffb5ac93fead34dccdf975fa73091
Dennis Griffin <dgdev1024@gmail.com>
2023-02-28 12:00:12 -0500

    Added a head tag to the home page and a title.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,21 @@
  * @file pages / index.tsx
  */
 
-import Container from '../com/container';
+import Head from "next/head";
+import Container from "../com/container";
 
 const HomePage = () => {
-  return <>
-    <Container />
-  </>
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>DG's Weather App</title>
+      </Head>
+      <Container />
+    </>
+  );
 };
 
 export default HomePage;
